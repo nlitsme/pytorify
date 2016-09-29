837681a8abfa460589fc0d6bd82f52a1627f3640
willem <itsme@xs4all.nl>
2016-09-29 11:08:22 +0200

    added setup, so torify can be installed as an egg

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+setup(
+    name = "pyTorify",
+    version = "1.0",
+    py_modules = ['torify'],
+    test_suite = 'test_torify',
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['PySocks>=1.5.6'],
+
+    # metadata for upload to PyPI
+    author = "Willem Hengeveld",
+    author_email = "itsme@xs4all.nl",
+    description = "Torify python scripts",
+    license = "MIT",
+    keywords = "TOR networking",
+    url = "https://github.com/nlitsme/pytorify/",
+
+    long_description = "Tool for redirecting all TCP traffic over TOR",
+    download_url = "",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        
+        'Topic :: Internet :: Proxy Servers',
+    ],
+
+)
